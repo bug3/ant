7587483e3b6caf1eca6fb1fc931d50a3b285bb94
bug3 <bug3dev@gmail.com>
2022-05-10 19:01:47 +0300

    Fix quotes

--- setup.sh
+++ setup.sh
@@ -18,9 +18,9 @@ function editGrub() {
     sudo sed -i '/GRUB_TIMEOUT=/d' $grub
     sudo sed -i '/GRUB_THEME=/d' $grub
 
-    sudo bash -c "echo 'GRUB_TIMEOUT_STYLE="menu"' >> $grub"
-    sudo bash -c "echo 'GRUB_TIMEOUT="6"' >> $grub"
-    sudo bash -c "echo "GRUB_THEME=\"$grubThemes/$theme/theme.txt\"" >> $grub"
+    sudo bash -c "echo 'GRUB_TIMEOUT_STYLE=\"menu\"' >> $grub"
+    sudo bash -c "echo 'GRUB_TIMEOUT=\"6\"' >> $grub"
+    sudo bash -c "echo 'GRUB_THEME=\"$grubThemes/$theme/theme.txt\"' >> $grub"
 }
 
 function updateGrub() {
