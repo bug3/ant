b6759926317554dff6a925fbde4d48e1740f240c
bug3 <bug3dev@gmail.com>
2022-05-10 18:33:07 +0300

    Define createGrub func

--- setup.sh
+++ setup.sh
@@ -3,3 +3,12 @@
 grub="/etc/default/grub"
 grubThemes="/usr/share/grub/themes"
 theme="ANT"
+
+function createGrub() {
+    if [[ ! -d $grubThemes/$theme ]]; then
+        sudo mkdir $grubThemes/$theme
+
+        sudo cp $grub $grub.bak
+        sudo cp -r $(pwd) $grubThemes
+    fi
+}
