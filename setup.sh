87484339ab1a09edfdd96e02b89c7c0fb02febd6
bug3 <bug3dev@gmail.com>
2022-05-10 18:34:57 +0300

    Define updateGrub func

--- setup.sh
+++ setup.sh
@@ -22,3 +22,17 @@ function editGrub() {
     sudo bash -c "echo 'GRUB_TIMEOUT="6"' >> $grub"
     sudo bash -c "echo "GRUB_THEME=\"$grubThemes/$theme/theme.txt\"" >> $grub"
 }
+
+function updateGrub() {
+    if [[ -x "$(command -v update-grub)" ]]; then
+        sudo update-grub
+    elif [[ -x "$(command -v grub-mkconfig)" ]]; then
+        sudo grub-mkconfig -o /boot/grub/grub.cfg
+    elif [[ -x "$(command -v grub2-mkconfig)" ]]; then
+        if [[ -x "$(command -v zypper)" ]]; then
+            sudo grub2-mkconfig -o /boot/grub2/grub.cfg
+        elif [[ -x "$(command -v dnf)" ]]; then
+            sudo grub2-mkconfig -o /boot/efi/EFI/fedora/grub.cfg
+        fi
+    fi
+}
